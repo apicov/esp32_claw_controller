d9a03b8e15f76e955b93ae737ad857d6335da71e
pico <a.pico@posteo.de>
2024-04-10 20:41:30 +0200

    add claw ctl topics to ros mqtt bridge

---
+++ include/private_data.h
@@ -0,0 +1,14 @@
+#define SSID "GL-AR300M-3f8-NOR"
+#define PASSWORD "goodlife"
+
+//#define SSID "picorete"
+//#define PASSWORD "62810666586883684317"
+
+//#define SSID "robonet"
+//#define PASSWORD "2kd84H/_67C2af"
+
+//const char* mqtt_broker = "192.168.178.37";
+const int mqtt_port = 1883;
+
+//const char* mqtt_broker = "192.168.1.101";
+const char* mqtt_broker = "192.168.8.132";
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include <Arduino.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+#include "private_data.h"
+
+int LED_BUILTIN = 2;
 
 const int pwmResolution = 8;  // PWM resolution (bits) - 8 bits for duty cycle
 const int pwmFrequency = 40000;  // PWM frequency in Hz
@@ -10,18 +16,29 @@ const int pwmChannelCrane2 = 1;  // PWM channel (0-15) on ESP32
 const int pwmCrane1Pin = 18;
 const int pwmCrane2Pin = 19;
 
-
-void clawUp(int speed);
-void clawDown(int speed);
-
-
-//top switch pin
+//top and bottom switches pins
 const int topSwitchPin = 35;
 const int bottomSwitchPin  = 34;
 
+
+void clawUp(int speed);
+void clawDown(int speed);
+void grabSequence(int speed, int graspStrength);
+
+
+//////
+const char* MQTT_CLIENT_NAME= "ESP32_CLAW";
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void mqtt_connect();
+void mqtt_subscriber_callback(char* topic, byte* payload, unsigned int length);
+void initWiFi();
+
+
 void setup() {
   Serial.begin(9600);
-  Serial.print("lalalalalalalaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+  Serial.print("start");
 
   pinMode(topSwitchPin, INPUT);
   pinMode(bottomSwitchPin, INPUT);
@@ -38,15 +55,41 @@ void setup() {
   int dutyCycle = 0;  
   ledcWrite(pwmChannelCrane1, dutyCycle);
   ledcWrite(pwmChannelCrane2, dutyCycle);
+
+
+  initWiFi();
+  Serial.println("Connected to the WiFi network");
+  // Set the MQTT server to the client
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(mqtt_subscriber_callback);
+  mqtt_connect();
+
+  client.subscribe("claw_ctl/cmd");
+
 }
 
 void loop() {
 
-clawUp(255);
-delay(3000);
-clawDown(255);
-delay(3000);
+  //connect to network in case there is no connection
+    if (WiFi.status() != WL_CONNECTED){
+      initWiFi();
+      Serial.print("wifi connected.");
+    }
+    
+    if (!client.connected()) {
+      mqtt_connect();
+      client.subscribe("claw_ctl/cmd");
+    }
 
+  client.loop();
+  
+
+  /*clawUp(255);
+  delay(3000);
+
+  clawDown(255);
+  delay(3000);
+*/
 }
 
 
@@ -60,7 +103,7 @@ void clawUp(int speed){
   do {
     pinState = digitalRead(topSwitchPin);
     Serial.println(pinState);
-    delay(20);
+    delay(10);
   } while (pinState == 1);
 
   ledcWrite(pwmChannelCrane1, 0);
@@ -80,4 +123,65 @@ void clawDown(int speed){
   } while (pinState == 1);
 
   ledcWrite(pwmChannelCrane2, 0);
+}
+
+void grabSequence(int speed, int graspStrength){
+  // move claw to the bottom
+  clawDown(speed);
+  delay(2000);
+
+  // close claw
+
+  // lift claw
+  clawUp(speed); 
+}
+
+
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.print('.');
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+
+void mqtt_connect() {
+  // Connect to MQTT Broker
+  while (!client.connected()) {
+      Serial.println("Connecting to MQTT Broker...");
+      if (client.connect("MQTT_CLIENT_NAME")) {
+          Serial.println("Connected to MQTT Broker");
+      } else {
+          Serial.print("Failed with state ");
+          Serial.print(client.state());
+          delay(5000);
+      }
+  }
+}
+
+
+void mqtt_subscriber_callback(char* topic, byte* payload, unsigned int length){
+  Serial.print("message received: ");
+
+  // convert payload to string
+  byte message[20];
+  memcpy(message, payload, length);
+  // add null character at the end
+  message[length] = '\0';
+  // convert to string
+  String cmd = (char*)message;
+  Serial.println(cmd);
+  
+  if ( cmd == "grab_seq"){
+      grabSequence(128,255);
+    }
+
 }
